22b140d1029ef5df408cd4b20f39c5299aac1b01
Lopes <hugoalexandrelopes2@gmail.com>
2022-01-04 11:33:07 +0100

    script to install and compile

---
+++ compile_samba.sh
@@ -0,0 +1,6 @@
+mkdir -p build
+TEGRA_KERNEL_OUT=$(pwd)/build/
+export CROSS_COMPILE=$HOME/l4t-gcc/gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+export LOCALVERSION=-tegra
+cd kernel-4.9
+make ARCH=arm64 O=$TEGRA_KERNEL_OUT Image
