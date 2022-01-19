63753761f461e0b6a086382d765f71dc354e14a3
Lopes <hugoalexandrelopes2@gmail.com>
2022-01-19 12:18:26 +0100

    new script

--- kernel/compile_insatll_samba.sh
+++ kernel/compile_insatll_samba.sh
@@ -5,15 +5,15 @@ export LOCALVERSION=-tegra
 cd kernel-4.9
 make ARCH=arm64 O=$TEGRA_KERNEL_OUT -j$(nproc)
 cd ..
-ln -s ~/nvidia/nvidia_sdk/JetPack_4.4.1_Linux_JETSON_AGX_XAVIER/Linux_for_Tegra Linux_for_Tegra
+ln -sf ~/nvidia/nvidia_sdk/JetPack_4.4.1_Linux_JETSON_AGX_XAVIER/Linux_for_Tegra 
 #first remove all the old file in sdkmanager 
-rm Linux_for_tegra/kernel/zImage
-rm Linux_for_tegra/kernel/Image
-rm Linux_for_tegra/kernel/dtb/tegra194-agx-cti-AGX101-JCB002-IMX390-8CAM.dtb
+rm Linux_for_Tegra/kernel/zImage
+rm Linux_for_Tegra/kernel/Image
+rm Linux_for_Tegra/kernel/dtb/tegra194-agx-cti-AGX101-JCB002-IMX390-8CAM.dtb
 
-cp DeviceTree/tegra194-agx-cti-AGX101-JCB002-IMX390-8CAM.dtb Linux_for_tegra/kernel/dtb/
-cp build/arch/arm64/boot/Image  Linux_for_tegra/kernel/
-cp build/arch/arm64/boot/zImage Linux_for_tegra/kernel/
+cp DeviceTree/tegra194-agx-cti-AGX101-JCB002-IMX390-8CAM.dtb Linux_for_Tegra/kernel/dtb/
+cp build/arch/arm64/boot/Image  Linux_for_Tegra/kernel/
+cp build/arch/arm64/boot/zImage Linux_for_Tegra/kernel/
 
 
 
