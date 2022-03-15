264559756d4024c8f50e893876a1792a2e949690
Lopes <hugoalexandrelopes2@gmail.com>
2022-03-15 08:39:50 +0100

    add script 1 camera and script i2c from user

---
+++ kernel/compile_dts_i2c.sh
@@ -0,0 +1,4 @@
+cd DeviceTree
+dtc -I dts -O dtb -f dts_i2c.dts -o tegra194-agx-cti-AGX101-JCB002-IMX390-8CAM.dtb
+cp tegra194-agx-cti-AGX101-JCB002-IMX390-8CAM.dtb ../Linux_for_Tegra/kernel/dtb/
+cd -
